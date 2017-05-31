804c137b15db947301482b720f9aa3d818b1f6e2
Andrew <andrewmbyrd@gmail.com>
2017-05-31 13:30:06 -0500

    Users can now post a review and rating for their game purchases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529163059) do
+ActiveRecord::Schema.define(version: 20170531152620) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170529163059) do
     t.integer  "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "review"
+    t.integer  "rating"
     t.index ["game_id"], name: "index_game_purchases_on_game_id"
     t.index ["library_id"], name: "index_game_purchases_on_library_id"
   end
