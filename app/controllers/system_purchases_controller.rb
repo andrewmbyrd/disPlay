1f01eef3e89e7d98b4b93027b19397d79893246a
Andrew <andrewmbyrd@gmail.com>
2017-05-31 14:40:47 -0500

    Fixed redirection and improved style

--- app/controllers/system_purchases_controller.rb
+++ app/controllers/system_purchases_controller.rb
@@ -5,7 +5,7 @@ class SystemPurchasesController < ApplicationController
     SystemPurchase.create!(library_id: current_user.id,
                            system_id: @system.id)
     flash[:notice] = "#{@system.name} added to your library!"
-    redirect_to [@system.company]
+    redirect_to [@system]
   end
 
 end
