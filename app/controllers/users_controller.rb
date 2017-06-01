0fb0975a00892b7687856d29853d0317f67bf350
Andrew <andrewmbyrd@gmail.com>
2017-06-01 14:38:49 -0500

    Complete userauthentication and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = current_user
   end
@@ -14,21 +16,31 @@ class UsersController < ApplicationController
     new_username = params[:user][:username]
     new_password = params[:user][:password_update]
     new_password_confirm = params[:user][:password_update_confirmation]
-    unless auth_password == @user.password
+    unless @user.valid_password?(auth_password)
       flash[:alert] = "Incorrect password"
       redirect_to users_edit_path
     else
-      if new_password == new_password_confirm && ! new_password.is_blank?
+      if new_password == new_password_confirm && !new_password.empty?
         @user.password = new_password
         notice += "Password updated. "
       end
-      unless new_username.is_blank
+      unless new_username.empty?
         @user.username = new_username
         notice += "Username updated."
       end
-      flash[:notice] = notice
-      redirect_to [@user]
+      begin
+        @user.save!
+      rescue ActiveRecord::RecordInvalid => e
+        flash[:alert] = e.message
+        redirect_to users_edit_path
+        return
+      end
+        bypass_sign_in(@user)
+        flash[:notice] = notice
+        redirect_to users_show_path
+
     end
   end
+
 
 end
