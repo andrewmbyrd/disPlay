8b66efcafc66e9f795944e1e68aaa4dbddeb841d
Andrew <andrewmbyrd@gmail.com>
2017-06-05 15:41:18 -0500

    Created assets for companies and systems

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,45 @@
 module CompaniesHelper
+  def logo_hash
+    {'Nintendo' => 'nintendo.png',
+     'Sega' => 'sega.png',
+     'Microsoft' => 'microsoft.png',
+     'Sony' => 'sony.png',
+     'Atari' => 'atari.png',
+     'SNK' => 'snk.png',
+     'Hudson Soft' =>  'hudson.png'
+     }
+  end
+
+  def system_logo_hash
+    {'Nintendo Entertainment System' => 'nes-icon.png',
+     'Game Boy' => 'gameboy.png',
+     'Super Nintendo Entertainment System' => 'snes.png',
+     'Nintendo 64' => 'n64.png',
+     'Game Boy Color' => 'gameboycolor.png',
+     'Game Boy Advance' => 'gba.png',
+     'GameCube' =>  'gcn.png',
+     'Nintendo DS' => 'nds.png',
+     'Wii' => 'wii.png',
+     'Wii U' => 'wiiu.png',
+     'Nintendo 3DS' => 'n3ds.png',
+     'Nintendo Switch' => 'switch.png',
+     'Genesis' => 'genesis.png',
+     'Game Gear' => 'gamegear.png',
+     'Sega CD' =>  'segacd.png',
+     'Sega Master System' => 'sms.png',
+     'Dreamcast' => 'dreamcast.png',
+     'Xbox' => 'xbox.ico',
+     'Xbox 360' => 'x360.png',
+     'Xbox One' => 'xbone.png',
+     'PlayStation' => 'ps.png',
+     'PlayStation 2' => 'ps2.png',
+     'PlayStation 3' =>  'ps3.ico',
+     'PlayStation Portable' => 'psp.ico',
+     'PlayStation Vita' => 'psv.png',
+     'PlayStation 4' => 'ps4.jpg',
+     'Atari 2600' => 'atari2600.png',
+     'Neo Geo' => 'neogeo.png',
+     'TurboGrafx-16' => 'turbog.png'
+    }
+  end
 end
