1bff611edb1efa35c6b5a72bb55c3e066884b37d
Andrew <andrewmbyrd@gmail.com>
2017-06-06 16:55:26 -0500

    Implemented comments in GamePurchased view and add them to Event list

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+
+  def create
+
+    purchase = GamePurchase.find(params[:id])
+    commented_user = purchase.library.user
+    Comment.create!(user_id: current_user.id,
+                    game_purchase_id: params[:id],
+                    body: params[:comment][:body])
+
+    Event.create!(user_id: current_user.id,
+                  game_purchase_id: params[:id],
+                  commentee: commented_user.id,
+                  action: "#{User.find(current_user.id).username} commented on #{commented_user.username}'s copy of ")
+    redirect_to library_game_purchase_path(purchase.library.id, purchase.id)
+  end
+
+end
--- app/models/game_purchase.rb
+++ app/models/game_purchase.rb
@@ -2,4 +2,5 @@ class GamePurchase < ApplicationRecord
   belongs_to :game
   belongs_to :library
   has_many :events
+  has_many :comments
 end
