12d86671b6225c0f8dd83dc5f7916232bff4cf89
Andrew <andrewmbyrd@gmail.com>
2017-06-12 15:02:48 -0500

    Use partials and helpers and reduce redundant css

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def num_following
+    @user.active_relationships.length
+  end
+
+  def num_followers
+    @user.passive_relationships.length
+  end
+
+  def relationship_for(user)
+    current_user.active_relationships.find_by(followed_id: user.id)
+  end
 end
