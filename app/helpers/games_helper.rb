a202fbc7ebef39abe54a3e275e7d8093345dae96
Andrew <andrewmbyrd@gmail.com>
2017-06-13 15:35:04 -0500

    optimized SQL queries

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -8,17 +8,9 @@ module GamesHelper
   end
 
   def avg_review_score(game)
-    sum = 0
-    num = 0
-    current_user.active_relationships.each do |rel|
-      followeds_game = User.find(rel.followed_id).library.game_purchases.find_by(game_id: @game.id)
-      if followeds_game
-        sum += followeds_game.rating if followeds_game.rating.is_a? Numeric
-        num += 1
-      end
-    end
-
-    num > 0 ? sum / num : nil
+    score = GamePurchase.where(game_id: game.id, library_id: current_user.active_relationships.pluck(:followed_id))
+                .average(:rating)
+    (score.is_a? BigDecimal) ? score.to_i : nil
   end
 
 
