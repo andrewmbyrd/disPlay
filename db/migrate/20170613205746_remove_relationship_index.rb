af29e5e244725b627b1249611c2cf3a0bc412bf2
Andrew <andrewmbyrd@gmail.com>
2017-06-15 11:40:00 -0500

    fix schema error

--- db/migrate/20170613205746_remove_relationship_index.rb
+++ db/migrate/20170613205746_remove_relationship_index.rb
@@ -1,5 +1,5 @@
 class RemoveRelationshipIndex < ActiveRecord::Migration[5.0]
   def change
-    remove_index :relationships, name: "index_relationships_on_follower_id_and_followed_id"
+    #remove_index :relationships, name: "index_relationships_on_follower_id_and_followed_id"
   end
 end
